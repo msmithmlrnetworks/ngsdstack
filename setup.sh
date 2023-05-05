b0997560544195e09c3d78e3bee355edfd30ac44
msmithmlrnetworks <msmith@mlrnetworks.co.uk>
2023-05-05 10:48:58 +0100

    exclude files from git

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 read -p "Enter the customer name: " new_value
 
 # # Remove leading/trailing spaces
@@ -16,4 +17,7 @@ sed "s|old_value|$new_value|g" "docker-compose.yml.template" > "docker-compose.y
 sed "s|old_value|$new_value|g" "ngrok.yaml.template" > "ngrok.yaml"
 
 
-echo "Value successfully updated! Now use the following command to start the container: sudo docker-compose up -d"
+echo "Value successfully updated! Now use the following commands to start the stack:"
+echo "'sudo apt install docker-compose'"
+echo "'sudo docker login registry.checkmk.com' and enter the credentials. Then:"
+echo "'sudo docker-compose up -d'"
