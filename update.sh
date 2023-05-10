f64ea228456debccd556726bbdf44115c1b8b308
msmithmlrnetworks <msmith@mlrnetworks.co.uk>
2023-05-10 15:45:59 +0100

    dd

--- update.sh
+++ update.sh
@@ -19,8 +19,8 @@ clean=${clean:0:16}
 # fi
 
 # Replace value in file
-sed "s|old_value|$new_value|g; s|old_engine_value|$clean_string|g" "docker-compose.yml.template" > "docker-compose.yml"
-sed "s|old_value|$new_value|g; s|old_engine_value|$clean_string|g" "ngrok.yaml.template" > "ngrok.yaml"
+sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "docker-compose.yml.template" > "docker-compose.yml"
+sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "ngrok.yaml.template" > "ngrok.yaml"
 echo $clean
 echo $new_value
 echo "Value successfully updated! Now use the following commands to start the stack:"
