979aaa5806a402601c911ead173e32c400bc9f72
msmithmlrnetworks <msmith@mlrnetworks.co.uk>
2023-06-27 15:08:22 +0100

    change order or setup script

--- setup.sh
+++ setup.sh
@@ -10,21 +10,22 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
   echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
   sudo tee /etc/apt/sources.list.d/ngrok.list && \
   sudo apt update && sudo apt install ngrok
-cp ./config/ngrok/ngrok.yml ~/.config/ngrok/ngrok.yml
-sudo ngrok service install --config ./config/ngrok/ngrok.yml
-sudo ngrok service start
-
 read -p "Enter the customer name EXACTLY as it appears in Vivantio: " new_value
 # Extract the first word before any spaces
 clean=$(echo "$new_value" | cut -d' ' -f1)
 
 # Remove special characters
 clean=$(echo "$clean" | tr -cd '[:alnum:]')
+sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "./config/ngrok/ngrok.yaml.template" > "./config/ngrok/ngrok.yaml"
+
+cp ./config/ngrok/ngrok.yml ~/.config/ngrok/ngrok.yml
+sudo ngrok service install --config ./config/ngrok/ngrok.yml
+sudo ngrok service start
+
 
 # Replace value in file
 sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "docker-compose.yml.template" > "docker-compose.yml"
 sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "ngrok.yaml.template" > "ngrok.yaml"
-sed "s|old_value|$new_value|g; s|old_engine_value|$clean|g" "./config/ngrok/ngrok.yaml.template" > "./config/ngrok/ngrok.yaml"
 echo $clean
 echo $new_value
 echo "Value successfully updated! Now use the following commands to start the stack:"
